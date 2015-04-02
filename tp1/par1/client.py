4610ec830e3f9452018f88ef5505d1afe560568a
Pedro Lula <plopes1991@gmail.com>
2015-04-02 19:43:36 -0300

    Random chars

--- tp1/par1/client.py
+++ tp1/par1/client.py
@@ -1,10 +1,15 @@
 import socket
+import random
+import string
+
 HOST = '10.0.0.1' # Endereco IP do Servidor
 PORT = 5000 # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
-ba = bytearray(1000)
+ba = []
+for x in range(1000)
+    ba[x] = random.choice(string.ascii_letters)
 for msg in ba:
     while reponse <> '0':
         tcp.send (msg)
