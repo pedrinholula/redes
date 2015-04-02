bcb78effbc569717ccd33730f8634eb65e88bfbb
Pedro Lula <plopes1991@gmail.com>
2015-04-02 19:53:12 -0300

    Changing the order of the server

--- tp1/par1/client.py
+++ tp1/par1/client.py
@@ -7,11 +7,12 @@ PORT = 5000 # Porta que o Servidor esta
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
+response = True
 ba = []
 for x in range(1000)
-    ba[x] = random.choice(string.ascii_letters)
-for msg in ba:
-    while reponse <> '0':
+    ba.append (random.choice(string.ascii_letters))
+while response <> '0':
+    for msg in ba:
         tcp.send (msg)
         response = tcp.recv(1024)
 tcp.close()
