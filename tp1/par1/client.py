1d3e8d61ccb6700f72270659d1d406cf86e3eb43
Pedro Lula <plopes1991@gmail.com>
2015-04-03 12:13:10 -0300

    Bugfix

--- tp1/par1/client.py
+++ tp1/par1/client.py
@@ -11,44 +11,9 @@ tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 
-tcp.send(""+MESSAGES)  # Envia o tamanho da mensagem
-reponse = tcp.recv(8)
-if response is True:
-    for x in range(MESSAGES):
-        while response is True:
-            msg = random.choice(string.ascii_letters)
-            tcp.send(msg)
-            response = tcp.recv(8)
-
-
-#Conecta com o servidor @TODO
-#Envia o tamanho da mensagem @TODO
-#Serivdor avisa que está pronto @TODO
-#Se o servidor está pronto @TODO
-    #Envia uma mensagem
-    #Se o servidor responde
-
-
-import socket
-import random
-import string
-
-HOST = '10.0.0.1'  # Endereco IP do Servidor
-PORT = 5000  # Porta que o Servidor esta
-MESSAGES = 10  # Numero de mensagens a ser enviado
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-
 for x in range(MESSAGES):
     msg = random.choice(string.ascii_letters)*10
     tcp.send(msg)
 
 response = tcp.recv(8)
 print response
-#Conecta com o servidor @TODO
-#Envia o tamanho da mensagem @TODO
-#Serivdor avisa que está pronto @TODO
-#Se o servidor está pronto @TODO
-    #Envia uma mensagem
-    #Se o servidor responde
--- tp1/par1/server.py
+++ tp1/par1/server.py
@@ -1,16 +1,18 @@
 # coding: utf-8
 
 import socket
+
 HOST = ''  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
+
 while True:
     con, cliente = tcp.accept()
     print 'Conectado por', cliente
-
     while True:
         msg = con.recv(1024)
         if not msg:
