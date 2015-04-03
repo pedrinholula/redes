04cd435a400999b197fc1d3f663cb79ce4304d20
Pedro Lula <plopes1991@gmail.com>
2015-04-02 22:31:20 -0300

    SS sen

--- tp1/par1/client.py
+++ tp1/par1/client.py
@@ -11,10 +11,11 @@ response = True
 ba = []
 for x in range(1000):
     ba.append (random.choice(string.ascii_letters))
+ss=StreamSocket(tcp,Raw)
 while response <> '0':
     for msg in ba:
         print msg
-        tcp.send (msg)
+        ss.send (Raw(msg))
         response = tcp.recv(1024)
         print response
 tcp.close()
