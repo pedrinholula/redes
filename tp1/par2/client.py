0b2c618485c2999c7194102c56d388c409aed7a6
Pedro Lula <plopes1991@gmail.com>
2015-04-03 13:48:47 -0300

    segundo par

--- tp1/par2/client.py
+++ tp1/par2/client.py
@@ -3,27 +3,31 @@
 import socket
 import random
 import string
+import sys
 
 HOST = '10.0.0.1'  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
 MESSAGES = 10  # Numero de mensagens a ser enviado
+SIZE = 10
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 dest = (HOST, PORT)
 tcp.connect(dest)
 
-tcp.send(""+MESSAGES)  # Envia o tamanho da mensagem
-reponse = tcp.recv(8)
-if response is True:
-    for x in range(MESSAGES):
-        while response is True:
-            msg = random.choice(string.ascii_letters)
-            tcp.send(msg)
-            response = tcp.recv(8)
+#Cria a lista de mensagens
+msg_lst = []
+for msg in range(MESSAGES):
+    w = ''
+    for size in range(SIZE):
+        w += random.choice(string.ascii_letters)
+    msg_lst.append(w)
 
-
-#Conecta com o servidor @TODO
-#Envia o tamanho da mensagem @TODO
-#Serivdor avisa que está pronto @TODO
-#Se o servidor está pronto @TODO
-    #Envia uma mensagem
-    #Se o servidor responde
+#Envia uma a uma as mensagens criadas
+response = True
+for msg in msg_lst:
+    print msg
+    while response:
+        tcp.send(msg)
+        response = tcp.recv()
+tcp.shutdown(socket.SHUT_RDWR)  # Fecha a conexão para envio
+tcp.close()
--- tp1/par2/server.py
+++ tp1/par2/server.py
@@ -1,20 +1,26 @@
 # coding: utf-8
 
 import socket
-HOST = '' # Endereco IP do Servidor
-PORT = 5000 # Porta que o Servidor esta
+
+HOST = ''  # Endereco IP do Servidor
+PORT = 5000  # Porta que o Servidor esta
+
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
 tcp.bind(orig)
 tcp.listen(1)
+print "Servidor iniciado. Aguardando conexão"
+# Execução infinita
 while True:
     con, cliente = tcp.accept()
     print 'Conectado por', cliente
     while True:
+        #Espera a mensagem do cliente
         msg = con.recv(1024)
-        if not msg: break
-        msg = '>' + msg + '<'
-        print cliente, msg
-        con.send(msg)
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
+        print msg
+        if not msg:
+            con.shutdown(socket.SHUT_RDWR)
+            con.close()
+            break
+        else:
+            con.send("0")  # envia uma mensagem
