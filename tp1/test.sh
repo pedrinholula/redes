e1dc93b217eb5b7d3cffb0f87a744d92020dd792
Pedro Lula <plopes1991@gmail.com>
2015-04-05 12:45:06 -0300

    test.sh

---
+++ tp1/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+for messages in 1 10 100 1000
+do
+  for size in 10 100 1000 10000
+  do
+    python src/par1/client.py $messages $size
+    python src/par2/client.py $messages $size
+  done
+done
