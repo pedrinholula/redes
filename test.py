0e724b73d805b0a1413ff98898659a4941bddc73
Pedro Lula <plopes1991@gmail.com>
2015-04-05 10:03:22 -0300

    Functions created

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import sys
 
 
 def create_messages(numM, sizeM):
-    #Cria a lista de mensagens
+#Cria a lista de mensagens
     msg_lst = []
     for msg in range(numM):
         w = ''
@@ -18,27 +18,15 @@ def create_messages(numM, sizeM):
 
 
 def client():
-    #Faz a conexão cliente servidor
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    dest = (HOST, PORT)
-    tcp.connect(dest)
     #Envia uma a uma as mensagens criadas
     for msg in create_messages(MESSAGES, SIZE):
         print msg
-        tcp.send(msg)
-    tcp.shutdown(socket.SHUT_WR)  # Fecha a conexão para envio
-    response = tcp.recv(8)  # Recebe a resposta do servidor
-    print response
-    tcp.close()
 
 
-HOST = '10.0.0.1'  # Endereco IP do Servidor
-PORT = 5000  # Porta que o Servidor esta
-#Numero de mensagens e tamanho de cada uma
 if len(sys.argv) == 3:
     MESSAGES = int(sys.argv[1])  # Numero de mensagens a ser enviado
     SIZE = int(sys.argv[2])
 else:
-    print "Usage: client.py <# MESSAGES> <MESSAGES SIZE>"
+    print "Usage: client.py <#MESSAGES> <MESSAGES SIZE>"
     sys.exit()
 client()
--- tp1/par2/client.py
+++ tp1/par2/client.py
@@ -5,37 +5,42 @@ import random
 import string
 import sys
 
+
+def create_messages(num, size):
+#Cria a lista de mensagens
+    msg_lst = []
+    for msg in range(num):
+        w = ''
+        for size in range(size):
+            w += random.choice(string.ascii_letters)
+        msg_lst.append(w)
+    return msg_lst
+
+
+def client():
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    dest = (HOST, PORT)
+    tcp.connect(dest)
+
+    #Envia uma a uma as mensagens criadas
+    while True:
+        for msg in create_messages():
+            tcp.send(msg)
+            response = tcp.recv(8)
+            print msg, response
+            if not response:
+                break
+        tcp.shutdown(socket.SHUT_RDWR)  # Fecha a conexão para envio
+        tcp.close()
+        break
+
+
 HOST = '10.0.0.1'  # Endereco IP do Servidor
 PORT = 5000  # Porta que o Servidor esta
-
 if len(sys.argv) == 3:
     MESSAGES = int(sys.argv[1])  # Numero de mensagens a ser enviado
     SIZE = int(sys.argv[2])
 else:
     print "Usage: client.py <# MESSAGES> <MESSAGES SIZE>"
     sys.exit()
-
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-
-#Cria a lista de mensagens
-msg_lst = []
-for msg in range(MESSAGES):
-    w = ''
-    for size in range(SIZE):
-        w += random.choice(string.ascii_letters)
-    msg_lst.append(w)
-
-#Envia uma a uma as mensagens criadas
-while True:
-    for msg in msg_lst:
-        tcp.send(msg)
-        response = tcp.recv(8)
-        print msg, response
-        if not response:
-            break
-    tcp.shutdown(socket.SHUT_RDWR)  # Fecha a conexão para envio
-    tcp.close()
-    break
+client()
